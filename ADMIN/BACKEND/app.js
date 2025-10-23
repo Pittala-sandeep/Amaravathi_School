bc737cee435a60619bfdbf44cd0d9e23a14bea9f
Sandeep Pittala <pittalasandeep473@gmail.com>
2025-10-23 21:16:50 +0530

    Fix deployment: add vite & nodemon, update scripts and path

--- ADMIN/BACKEND/app.js
+++ ADMIN/BACKEND/app.js
@@ -16,9 +16,7 @@ const app = express();
 
 
 const path = require("path");
-const { fileURLToPath } =  require("url");
 
-const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve the built frontend
