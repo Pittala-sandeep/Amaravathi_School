dfc31c02d8f0f5057b9aca798dc646d21e68eb10
Sandeep Pittala <pittalasandeep473@gmail.com>
2025-10-23 21:45:50 +0530

    Fix CORS origins for Render deployment

--- ADMIN/BACKEND/app.js
+++ ADMIN/BACKEND/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: [process.env.FRONTEND_ORIGIN, process.env.ADMIN_FRONTEND_ORIGIN || ""].filter(Boolean),
+  origin: [process.env.FRONTEND_ORIGIN],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
